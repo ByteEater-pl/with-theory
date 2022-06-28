82d96d6cf036de5c9e434919ad919d1e27ab1d60
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2022-06-28 05:34:59 +0200

    Create Codeworld.hs

---
+++ Materiały do sezonu #1/Codeworld.hs
@@ -0,0 +1,5 @@
+program = drawingOf(picture)
+picture1 = rectangle(6, 10)
+picture2 = circle(5)
+picture3 = translated(picture2, 3, -1)
+picture = pictures [picture1, picture3, colored(picture2, red)]
